9cad29a444062fced862a2ced2f3f7050588a19a
Brandon Baker <brandonbaker40@icloud.com>
2017-03-10 16:48:54 -0500

    adds moderator role.... troubleshooting

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -2,4 +2,8 @@ module TopicsHelper
   def user_is_authorized_for_topics?
         current_user && current_user.admin?
    end
+   #assignment atttempt
+   def user_is_authorized_for_topics_edit?
+      current_user && (current_user.moderator? || current_user.admin?)
+   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -106,12 +106,20 @@ posts = SponsoredPost.all
    role:     'admin'
  )
 
+ moderator = User.create!(
+    name:     'Moderator User',
+    email:    'moderator@example.com',
+    password: 'helloworld',
+    role:     'moderator'
+ )
+
  # Create a member
  member = User.create!(
    name:     'Member User',
    email:    'member@example.com',
    password: 'helloworld'
  )
+
 
 puts "Seed finished"
 puts "#{User.count} users created"
