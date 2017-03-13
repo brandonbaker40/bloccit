0c9d9127a01b99a69321a19dd26e031f1f39d790
Brandon Baker <brandonbaker40@icloud.com>
2017-03-13 10:09:54 -0400

    adds moderator role... fixes spec but not controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,9 @@ class PostsController < ApplicationController
 
   before_action :authorize_user, except: [:show, :new, :create]
 
+  #added in
+#  before_action :authorize_moderator, only: [:new, :create, :edit, :update]
+
   def show
 
     @post = Post.find(params[:id])
@@ -23,7 +26,6 @@ class PostsController < ApplicationController
       flash[:notice] = "Post was saved."
       redirect_to [@topic, @post]
     else
-
       flash.now[:alert] = "There was an error saving the post. Please try again."
       render :new
     end
@@ -70,5 +72,13 @@ class PostsController < ApplicationController
        flash[:alert] = "You must be an admin to do that."
        redirect_to [post.topic, post]
      end
+   end
+   ##### added in
+   def authorize_moderator
+     post = Post.find(params[:id])
+      unless current_user == post.user || current_user.moderator? || current_user.admin?
+        flash[:alert] = "You must be a moderator or admin to do that."
+        redirect_to [post.topic, post]
+      end
    end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,10 @@
 module PostsHelper
   def user_is_authorized_for_post?(post)
-        current_user && (current_user == post.user || current_user.admin?)
-   end
+    current_user && (current_user == post.user || current_user.admin?)
+  end
 
-   def user_is_authorized_for_post_mod?(post)
-         current_user && (current_user == post.user || current_user.moderator? || current_user.admin?)
-    end
+  def user_is_authorized_for_post_mod?(post)
+    current_user && (current_user == post.user || current_user.moderator? || current_user.admin?)
+  end
+  
 end
