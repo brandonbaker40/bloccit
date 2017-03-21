ffec9b0ea4739968349de31807c145b97b7492bd
Brandon Baker <brandonbaker40@icloud.com>
2017-03-21 10:50:23 -0400

    favorite mailer sending... troubleshooting

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -2,14 +2,23 @@ class FavoriteMailer < ApplicationMailer
   default from: "brbaker400@gmail.com"
 
   def new_comment(user, post, comment)
-    headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
-    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
-    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+    headers["Message-ID"] = "<comments/#{comment.id}@still-bayou-19051>"
+    headers["In-Reply-To"] = "<post/#{post.id}@still-bayou-19051>"
+    headers["References"] = "<post/#{post.id}@still-bayou-19051>"
 
     @user = user
     @post = post
     @comment = comment
 
     mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+
+ def new_post(post)
+   headers["Message-ID"] = "<posts/#{post.id}@still-bayou-19051>"
+   headers["In-Reply-To"] = "<posts/#{post.id}@still-bayou-19051>"
+   headers["References"] = "<post/#{post.id}@still-bayou-19051>"
+
+   @post = post
+   mail(to: post.user.email, subject: "You're following, #{post.title}!")
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,7 @@ class Post < ActiveRecord::Base
   has_many :favorites, dependent: :destroy
 
   after_create :create_vote
+  after_create :create_favorite
 
   default_scope { order('rank DESC') }
 
@@ -42,5 +43,10 @@ class Post < ActiveRecord::Base
 
    def create_vote
     user.votes.create(value: 1, post: self)
+   end
+
+   def create_favorite
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
    end
 end
